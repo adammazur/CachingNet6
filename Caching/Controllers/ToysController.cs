9aee5fdeb8b1a923f959c2bb6ef6b5bc0d711736
Adam Mazur <adam.mazur@currenda.pl>
2022-07-21 14:19:45 +0200

    caching in memory for toys list

--- Caching/Controllers/ToysController.cs
+++ Caching/Controllers/ToysController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Caching.Data;
 using Caching.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Caching.Controllers
 {
@@ -9,22 +10,37 @@ namespace Caching.Controllers
     [ApiController]
     public class ToysController : ControllerBase
     {
+        private readonly IMemoryCache _memoryCache;
         private readonly CachingContext _context;
 
-        public ToysController(CachingContext context)
+        public ToysController(CachingContext context, IMemoryCache memoryCache)
         {
             _context = context;
+            _memoryCache = memoryCache;
         }
 
         // GET: api/Toys
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Toy>>> GetToys()
         {
-          if (_context.Toys == null)
-          {
-              return NotFound();
-          }
-            return await _context.Toys.ToListAsync();
+            var cacheKey = "toys";
+
+            // get value from memory cache
+            if (!_memoryCache.TryGetValue(cacheKey, out List<Toy> toys))
+            {
+                toys = await _context.Toys.ToListAsync();
+                var cacheExpiryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                    Priority = CacheItemPriority.High,
+                    SlidingExpiration = TimeSpan.FromMinutes(2)
+                };
+                
+                // set cache value by key 'toys'
+                _memoryCache.Set(cacheKey, toys, cacheExpiryOptions);
+            }
+
+            return Ok(toys);
         }
     }
 }
